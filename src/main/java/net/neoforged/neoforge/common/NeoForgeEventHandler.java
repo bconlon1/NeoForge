ef2749b85eaf15e5c443045167a6624f0a75d83c
bconlon <bconlon.gml@gmail.com>
2025-01-14 14:14:42 -0800

    simplified recipe priority system with a nonfinal byType map

--- src/main/java/net/neoforged/neoforge/common/NeoForgeEventHandler.java
+++ src/main/java/net/neoforged/neoforge/common/NeoForgeEventHandler.java
@@ -23,6 +23,7 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.LogicalSide;
+import net.neoforged.neoforge.common.crafting.RecipePriorityManager;
 import net.neoforged.neoforge.common.loot.LootModifierManager;
 import net.neoforged.neoforge.common.util.FakePlayerFactory;
 import net.neoforged.neoforge.common.util.LogicalSidedProvider;
@@ -147,12 +148,15 @@ public class NeoForgeEventHandler {
     }
 
     private static LootModifierManager INSTANCE;
+    private static RecipePriorityManager RECIPE_PRIORITY;
     private static DataMapLoader DATA_MAPS;
 
     @SubscribeEvent
     public void onResourceReload(AddReloadListenerEvent event) {
         INSTANCE = new LootModifierManager();
+        RECIPE_PRIORITY = new RecipePriorityManager();
         event.addListener(INSTANCE);
+        event.addListener(RECIPE_PRIORITY);
         event.addListener(DATA_MAPS = new DataMapLoader(event.getConditionContext(), event.getRegistryAccess()));
     }
 
@@ -160,6 +164,12 @@ public class NeoForgeEventHandler {
         if (INSTANCE == null)
             throw new IllegalStateException("Can not retrieve LootModifierManager until resources have loaded once.");
         return INSTANCE;
+    }
+
+    public static RecipePriorityManager getRecipePriorityManager() {
+        if (RECIPE_PRIORITY == null)
+            throw new IllegalStateException("Can not retrieve RecipePriorityManager until resources have loaded once.");
+        return RECIPE_PRIORITY;
     }
 
     @SubscribeEvent
